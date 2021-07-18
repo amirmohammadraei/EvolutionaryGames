255d82fbe7d6ede64ec2ab148bb030181c901e29
Amirmohammad Raei <amir.m.raei@gmail.com>
2021-07-18 23:35:27 +0430

    nn fertig

--- nn.py
+++ nn.py
@@ -4,19 +4,17 @@ import numpy as np
 class NeuralNetwork():
 
     def __init__(self, layer_sizes):
-
-        self.w1 = np.random.normal(size=(layer_sizes[1], layer_sizes[0]))
-        self.b1 = np.zeros((layer_sizes[1], 1))
-        self.w2 = np.random.normal(size=(layer_sizes[2], layer_sizes[1]))
-        self.b2 = np.zeros((layer_sizes[2], 1))
-        self.y = 0
+        self.w1 = np.random.randn(layer_sizes[0], layer_sizes[1])
+        self.b1 = np.random.randn(layer_sizes[1], 1)
+        self.w2 = np.random.randn(layer_sizes[1], layer_sizes[2])
+        self.b2 = np.random.randn(layer_sizes[2], 1)
 
     def activation(self, x):
         return 1 / (1 + np.exp(-x))
 
     def forward(self, x):
-        z1 = np.dot(self.w1, x) + self.b1
-        z1 = self.activation(z1)
-        z2 = np.dot(self.w2, z1) + self.b2
-        z2 = self.activation(z2)
-        return z2
+        z1 = self.w1.T @ x + self.b1
+        a1 = self.activation(z1)
+        z2 = self.w2.T @ a1 + self.b2
+        a2 = self.activation(z2)
+        return a2
